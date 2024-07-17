e961cb06886f2154be8d94cc95f030dc5e16e55a
Mehradi <mehradi.info@gmail.com>
2024-07-17 09:17:08 +0200

    Handling confirm

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -4,7 +4,7 @@ context("Actions", () => {
   beforeEach(() => {
     //cy.visit("https://example.cypress.io/commands/actions");
   });
-  it.only("", () => {
+  it("Handling alerts", () => {
     cy.visit("https://webdriveruniversity.com");
     cy.get("#popup-alerts")
       .invoke("removeAttr", "target")
@@ -13,5 +13,17 @@ context("Actions", () => {
     cy.on("window:alert", (str) => {
       expect(str).to.equal("I am an alert box!");
     });
+  });
+  it.only("Handling confirm", () => {
+    cy.visit("https://webdriveruniversity.com");
+    cy.get("#popup-alerts")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+    cy.get("#button4").click();
+    cy.on("window:confirm", (str) => {
+      // expect(str).to.equal("I am an alert box!");
+      return false;
+    });
+    cy.get("#confirm-alert-text").contains("You pressed Cancel!");
   });
 });
